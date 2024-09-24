af5f03d82e60c4a230ba28e6fd93d2d7944b6f5e
Josh Honig <josh@honig.email>
2024-09-24 16:01:48 -0500

    push existing code

---
+++ check.js
@@ -0,0 +1,44 @@
+function warnInvalid(reason) {
+  alert("Bad API key! " + reason);
+  document.cookie = "apikey=; expires="+ new Date(0).toUTCString(); // force apikey cookie to expire
+  window.location.href = "/";
+}
+
+// Search document.cookie for the API key.
+var match = document.cookie.match(new RegExp("(^| )apikey=([^;]+)"));
+let apikey;
+if (match) {
+  apikey = match[2];
+} else {
+  warnInvalid("No API key specified.");
+}
+// Test the given API key by attempting to fetch the Astronomy Picture of the Day.
+var test = "";
+fetch("https://api.nasa.gov/planetary/apod?api_key=" + apikey)
+  .then((function (response) {
+    return response.json();
+  }))
+  .then(function (response) {
+    console.log(response);
+    if (response.hasOwnProperty("error")){
+        // If API response has "error" key.
+        warnInvalid('API response: \n"' + response['error']['message'] + '"');
+    } else if (response.hasOwnProperty("copyright") && response.hasOwnProperty("url")) {
+        // If API response contains the expected keys...
+        // Store the keys locally
+        localStorage.setItem("apod.hdurl", response['hdurl']);
+        localStorage.setItem("apod.copyright", response['copyright']);
+        localStorage.setItem("apod.explanation", response['explanation']);
+        localStorage.setItem("apod.title", response['title']);
+        // Set the background image
+        //document.body.style.backgroundImage = "url('" + response['hdurl'] + "')"; 
+        // Show success text
+        document.getElementById("textWait").style.display = "none";
+        document.getElementById("textSuccess").style.display = "block";
+    } else {
+        warnInvalid("API returned unexpected response.")
+    }
+  })
+  .catch(function (err) {
+    warnInvalid("Error connecting to the NASA API: " + err);
+  });
---
+++ index.js
@@ -0,0 +1,15 @@
+function setCookie(name, value) {
+  var date = new Date();
+  date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+  var expires = "; expires=" + date.toUTCString();
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+let loginForm = document.getElementById("apiForm");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent the form from submitting
+  /* THE FOLLOWING IS REALLY REALLY DANGEROUS AND SHOULD NEVER BE DONE IN THE REAL WORLD! 
+       Storing sensitive information client-side is a HUGE no-no. It pains me to do this. */
+  setCookie("apikey", document.getElementById("apiKeyTextbox").value);
+  window.location.href = "/check.html";
+});
