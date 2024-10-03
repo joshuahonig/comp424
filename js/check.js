d11c970e526e4eeb75ef704deb875f976bde4c62
trosario102 <t.rosario102@gmail.com>
2024-10-03 12:20:34 -0500

    Added redirect to rover.html from check.html; home page is rover.html

--- js/check.js
+++ js/check.js
@@ -1,6 +1,6 @@
 function warnInvalid(reason) {
   alert("Bad API key! " + reason);
-  document.cookie = "apikey=; expires="+ new Date(0).toUTCString(); // force apikey cookie to expire
+  document.cookie = "apikey=; expires=" + new Date(0).toUTCString(); // force apikey cookie to expire
   window.location.href = "/";
 }
 
@@ -20,21 +20,22 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=" + apikey)
   }))
   .then(function (response) {
     console.log(response);
-    if (response.hasOwnProperty("error")){
-        // If API response has "error" key.
-        warnInvalid('API response: \n"' + response['error']['message'] + '"');
+    if (response.hasOwnProperty("error")) {
+      // If API response has "error" key.
+      warnInvalid('API response: \n"' + response['error']['message'] + '"');
     } else if (response.hasOwnProperty("url")) {
-        // If API response contains the expected keys...
-        // Store the keys locally
-        localStorage.setItem("apod.url", response['url']);
-        localStorage.setItem("apod.title", response['title']);
-        // Set the background image
-        //document.body.style.backgroundImage = "url('" + response['hdurl'] + "')"; 
-        // Show success text
-        document.getElementById("textWait").style.display = "none";
-        document.getElementById("textSuccess").style.display = "block";
+      // If API response contains the expected keys...
+      // Store the keys locally
+      localStorage.setItem("apod.url", response['url']);
+      localStorage.setItem("apod.title", response['title']);
+      // Set the background image
+      //document.body.style.backgroundImage = "url('" + response['hdurl'] + "')"; 
+      // Show success text
+      document.getElementById("textWait").style.display = "none";
+      document.getElementById("textSuccess").style.display = "block";
+      window.location.href = "/rover.html";
     } else {
-        warnInvalid("API returned unexpected response.")
+      warnInvalid("API returned unexpected response.")
     }
   })
   .catch(function (err) {
