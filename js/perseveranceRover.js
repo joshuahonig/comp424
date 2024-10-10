ba006c7b0f2004bc09bc98f82a26a2698ed8282f
trosario102 <t.rosario102@gmail.com>
2024-10-10 18:08:07 -0500

    Added date search and progress bar  to each rover js file

--- js/perseveranceRover.js
+++ js/perseveranceRover.js
@@ -7,6 +7,38 @@ perseveranceToggle.addEventListener('click', () => {
     if (perseveranceForm.getAttribute('data-visible') === 'false') {
         perseveranceForm.setAttribute('data-visible', 'true');
         perseveranceForm.style.display = 'block';
+
+        var roverSelect = document.getElementById("roverSelect4");
+        var selectedRover = roverSelect.options[roverSelect.selectedIndex].value;
+
+        if (document.getElementById("dateCaption4").innerHTML) {
+            document.getElementById("loadingBar4").style.display = "none";
+        } else {
+            document.getElementById("loadingBar4").style.display = "block";
+        }
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${apikey}`)
+
+            .then((function (response) {
+                return response.json();
+            }))
+            .then(function (response) {
+                for (var x = 0; x < response['rovers'].length; x++) {
+                    if (response['rovers'][x]['name'].toUpperCase() == selectedRover.toUpperCase()) {
+                        console.log(response['rovers'][x]);
+                        var oldest = response['rovers'][x]['landing_date'];
+                        var newest = response['rovers'][x]['max_date'];
+                        document.getElementById("searchDate4").min = oldest;
+                        document.getElementById("searchDate4").max = newest;
+                        document.getElementById("dateCaption4").innerHTML = `Range of available dates: ${oldest} to ${newest}`
+                    }
+                }
+                document.getElementById("loadingBar4").style.display = "none";
+            })
+            .catch(function (err) {
+                warnInvalid("Error connecting to the NASA API: " + err);
+            });
+
+
     } else {
         perseveranceForm.setAttribute('data-visible', 'false');
         perseveranceForm.style.display = 'none'
--- js/spiritRover.js
+++ js/spiritRover.js
@@ -7,6 +7,39 @@ spiritToggle.addEventListener('click', () => {
     if (spiritForm.getAttribute('data-visible') === 'false') {
         spiritForm.setAttribute('data-visible', 'true');
         spiritForm.style.display = 'block';
+
+        var roverSelect = document.getElementById("roverSelect2");
+        var selectedRover = roverSelect.options[roverSelect.selectedIndex].value;
+
+        if (document.getElementById("dateCaption2").innerHTML) {
+            document.getElementById("loadingBar2").style.display = "none";
+        } else {
+            document.getElementById("loadingBar2").style.display = "block";
+        }
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${apikey}`)
+
+            .then((function (response) {
+                return response.json();
+            }))
+            .then(function (response) {
+                for (var x = 0; x < response['rovers'].length; x++) {
+                    if (response['rovers'][x]['name'].toUpperCase() == selectedRover.toUpperCase()) {
+                        console.log(response['rovers'][x]);
+                        var oldest = response['rovers'][x]['landing_date'];
+                        var newest = response['rovers'][x]['max_date'];
+                        document.getElementById("searchDate2").min = oldest;
+                        document.getElementById("searchDate2").max = newest;
+                        document.getElementById("dateCaption2").innerHTML = `Range of available dates: ${oldest} to ${newest}`
+                    }
+                }
+                document.getElementById("loadingBar2").style.display = "none";
+            })
+            .catch(function (err) {
+                warnInvalid("Error connecting to the NASA API: " + err);
+            });
+
+
+
     } else {
         spiritForm.setAttribute('data-visible', 'false');
         spiritForm.style.display = 'none'
