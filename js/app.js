5e00d226e1644728fbca6475c91cdf986283623d
joshuahonig <josh@honig.email>
2024-11-26 19:29:11 -0600

    globalize navbar

--- js/app.js
+++ js/app.js
@@ -15,17 +15,40 @@ if (typeof authNotRequired === "undefined") {
     warnInvalid("No API key specified.");
   }
 
-  var logoutBtn = document.getElementById("logoutLink");
-  if (logoutBtn !== null) {
-    logoutBtn.onclick = function () {
-      localStorage.removeItem("apikey");
-      window.location.href = "/index.html";
-    };
-  }
   console.log("Loaded API");
 } else {
   console.log("Page has no auth; not loading API");
 }
+
+/* Load the navbar. */
+var navbarRequest = new XMLHttpRequest();
+navbarRequest.open("GET", "/parts/nav.html", true);
+navbarRequest.onload = function () {
+  if (navbarRequest.status >= 200 && navbarRequest.status < 400) {
+
+    /* Set the navbar element content. */
+    document.getElementById("navbar").innerHTML = navbarRequest.responseText;
+
+    /* Now that the navbar is loaded, set up the logout button. */
+    var logoutBtn = document.getElementById("logoutLink");
+    if (logoutBtn !== null) {
+      logoutBtn.onclick = function () {
+        localStorage.removeItem("apikey");
+        window.location.href = "/index.html";
+      }
+    } 
+    /* If we're not logged in, don't show buttons that require auth. */
+    if (localStorage.getItem("apikey") === null){
+      document.getElementById("roverLink").style.display = "none";
+      document.getElementById("searchLink").style.display = "none";
+      document.getElementById("aboutLink").style.display = "none";
+      document.getElementById("logoutLink").style.display = "none";
+    }
+
+
+  }
+};
+navbarRequest.send();
 
 /* todo: implement this on pages beyond rover.html, implement navbar in this script and beyond rover.html; 
          add logic to navbar code that hides logout if not logged in */
