c216e451e7dad2a30f86278b85ade264ca7a3660
trosario102 <t.rosario102@gmail.com>
2024-12-01 21:46:31 -0600

    Added double click events. Single click event not working

--- js/apod.js
+++ js/apod.js
@@ -48,3 +48,20 @@ window.addEventListener('load', function () {
         apodContainer.style.display = "block";
     }
 });
+
+let clickTimer;
+
+apodImage.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    clearTimeout(clickTimer);
+    clickTimer = setTimeout(() => {
+        //TODO: put code that brings up full page viewer
+    }, 200);
+
+});
+
+apodImage.addEventListener("dblclick", function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    clearTimeout(clickTimer);
+    window.open(apodImage.src, "_blank");
+});
