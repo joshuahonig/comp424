9ba416ff6ece5e1359191bbd51ee0dbfa4defadf
joshuahonig <josh@honig.email>
2024-12-03 14:45:13 -0600

    add href to logo; add redirect for already logged in users from login page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
-function setCookie(name, value) {
-  var date = new Date();
-  date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
-  var expires = "; expires=" + date.toUTCString();
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
 let loginForm = document.getElementById("apiForm");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault(); // prevent the form from submitting
-  /* THE FOLLOWING IS REALLY REALLY DANGEROUS AND SHOULD NEVER BE DONE IN THE REAL WORLD! 
-       Storing sensitive information client-side is a HUGE no-no. It pains me to do this. */
   localStorage.setItem("apikey", document.getElementById("apiKeyTextbox").value);
   window.location.href = "/check.html";
 });
+
+if (localStorage.getItem("apikey") !== null) {
+  // User is already logged in. Redirect.
+  window.location.href = "/rover.html"
+}
