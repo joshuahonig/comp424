1ef8de4347ee0023b3ff34c7ce9967f44d8ae23c
trosario102 <t.rosario102@gmail.com>
2024-12-03 13:36:32 -0600

    removed toggling from rover page

--- js/rover.js
+++ js/rover.js
@@ -1,20 +1,3 @@
-// toggle switch
-let oppToggle = document.getElementById('opportunity');
-// get node in DOM
-let oppForm = document.getElementById('opportunityForm');
-
-oppToggle.addEventListener('click', () => {
-    if (oppForm.getAttribute('data-visible') === 'false') {
-        oppForm.setAttribute('data-visible', 'true');
-        oppForm.style.display = 'block';
-
-    } else {
-        oppForm.setAttribute('data-visible', 'false');
-        oppForm.style.display = 'none'
-    }
-
-});
-
 oppForm.addEventListener('submit', () => {
     var form = new FormData(oppForm);
     var rov = form.get("roverSelect1");
@@ -26,23 +9,6 @@ oppForm.addEventListener('submit', () => {
     window.location.href = "search.html";
 });
 
-
-// toggle switch
-let spiritToggle = document.getElementById('spirit');
-// get node in DOM
-let spiritForm = document.getElementById('spiritForm');
-
-spiritToggle.addEventListener('click', () => {
-    if (spiritForm.getAttribute('data-visible') === 'false') {
-        spiritForm.setAttribute('data-visible', 'true');
-        spiritForm.style.display = 'block';
-
-    } else {
-        spiritForm.setAttribute('data-visible', 'false');
-        spiritForm.style.display = 'none'
-    }
-
-});
 
 spiritForm.addEventListener('submit', () => {
     var form = new FormData(spiritForm);
@@ -56,23 +22,6 @@ spiritForm.addEventListener('submit', () => {
 });
 
 
-// toggle switch
-let curiosityToggle = document.getElementById('curiosity');
-// get node in DOM
-let curiosityForm = document.getElementById('curiosityForm');
-
-curiosityToggle.addEventListener('click', () => {
-    if (curiosityForm.getAttribute('data-visible') === 'false') {
-        curiosityForm.setAttribute('data-visible', 'true');
-        curiosityForm.style.display = 'block';
-
-    } else {
-        curiosityForm.setAttribute('data-visible', 'false');
-        curiosityForm.style.display = 'none'
-    }
-
-});
-
 curiosityForm.addEventListener('submit', () => {
     var form = new FormData(curiosityForm);
     var rov = form.get("roverSelect3");
@@ -84,23 +33,6 @@ curiosityForm.addEventListener('submit', () => {
     window.location.href = "search.html";
 });
 
-
-// toggle switch
-let perseveranceToggle = document.getElementById('perseverance');
-// get node in DOM
-let perseveranceForm = document.getElementById('perseveranceForm');
-
-perseveranceToggle.addEventListener('click', () => {
-    if (perseveranceForm.getAttribute('data-visible') === 'false') {
-        perseveranceForm.setAttribute('data-visible', 'true');
-        perseveranceForm.style.display = 'block';
-
-    } else {
-        perseveranceForm.setAttribute('data-visible', 'false');
-        perseveranceForm.style.display = 'none'
-    }
-
-});
 
 perseveranceForm.addEventListener('submit', () => {
     var form = new FormData(perseveranceForm);
